8cfbbd06c285a840210ab3e80a6d91c58bc8d500
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-04-21 21:37:03 +0200

    fixed path bug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let landingButton = document.getElementById('explore')
 
 landingButton.addEventListener('mouseover', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('../images/bg.gif')"
+	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
 	
 })
 
 landingButton.addEventListener('mouseout', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('../images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
+	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
 })
