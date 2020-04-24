a12355014a91aceb69c061196c8ef1f9b6797e72
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-04-24 16:53:32 +0200

    added play function to play buttons in top tracks section and changed the player bar design

---
+++ landingpagescript.js
@@ -0,0 +1,11 @@
+let landingButton = document.getElementById('explore')
+
+//change background image to gif upon hovering over buttton
+landingButton.addEventListener('mouseover', function(){
+	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
+	
+})
+
+landingButton.addEventListener('mouseout', function(){
+	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
+})
--- script.js
+++ script.js
@@ -1,10 +1,89 @@
-let landingButton = document.getElementById('explore')
+// on click play the song
+let playing = true;
+let song = document.getElementById('audio');
+let songTitle = document.getElementById('songTitle');
+let artist = document.getElementById('artistName');
+let progressBar = document.getElementById('progressBar');
 
-landingButton.addEventListener('mouseover', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
-	
+let songIndex = 0;
+let songs = ['music/bensound-energy.mp3', 'music/bensound-hey.mp3', 'music/bensound-perception.mp3']
+songTitles = ["Lush Life", "Ruin My Life", "All the Time"];
+
+let playTrack = document.getElementsByClassName('play-track')
+
+const playOrPause = () => {
+  console.log(songIndex)
+  if (playing) {
+  	console.log(songIndex)
+  	document.getElementById('playButton').style.display = 'none';
+  	document.getElementById('pauseButton').style.display = 'inline-block';
+  	song.src = songs[songIndex];
+    song.play();
+    songTitle.innerHTML = songTitles[songIndex];
+    artist.innerHTML = 'Zara Larsson' 
+    playing = false;
+
+  } else {
+  	document.getElementById('pauseButton').style.display = 'none';
+  	document.getElementById('playButton').style.display = 'inline-block';
+  	song.pause();
+  	songTitle.innerHTML = songTitles[songIndex];
+    artist.innerHTML = 'Zara Larsson'
+    playing = true;
+    console.log(songIndex)
+  }
+}
+
+song.addEventListener('ended', function(){
+	nextSong()
 })
 
-landingButton.addEventListener('mouseout', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
-})
+const nextSong = () => {
+	songIndex++;
+	if (songIndex > 2){
+		songIndex = 0;
+	}
+
+	song.src = songs[songIndex];
+	songTitle.innerHTML = songTitles[songIndex];
+
+	playing = true;
+	playOrPause(songIndex)
+}
+
+const previousSong = () => {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = 2;
+    };
+
+    song.src = songs[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
+
+    playing = true;
+    playOrPause(songIndex);
+}
+
+// update progressBar.max to display song duration
+const updateProgressValue = () => {
+    progressBar.max = song.duration;
+    progressBar.value = song.currentTime;
+};
+
+// update the progress bar as song goes on
+setInterval(updateProgressValue, 500);
+
+// change progressBar.value when dragging the progress bar slider
+const changeProgressBar = () => {
+    song.currentTime = progressBar.value;
+}
+
+// add event listener to every button in top tracks section
+
+for (let i = 0; i < playTrack.length; i++){
+	playTrack[i].addEventListener('click', function(){
+		songIndex = i
+		console.log(songIndex)
+		playOrPause()
+	})
+}
