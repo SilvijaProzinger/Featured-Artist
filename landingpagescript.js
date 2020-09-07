555b11800c2e2e633a074411e52690b226f2daba
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-09-07 12:23:54 +0200

    fixed indentation

--- landingpagescript.js
+++ landingpagescript.js
@@ -2,10 +2,10 @@ let landingButton = document.getElementById('explore')
 
 //change background image to gif upon hovering over buttton
 landingButton.addEventListener('mouseover', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
+ document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
 	
 })
 
 landingButton.addEventListener('mouseout', function(){
-	document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
+ document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
 })
--- script.js
+++ script.js
@@ -15,20 +15,20 @@ let volumeSlider = document.getElementById('volumeSlider')
 const playOrPause = () => {
   console.log(songIndex)
   if (playing) {
-  	console.log(songIndex)
-  	document.getElementById('playButton').style.display = 'none';
-  	document.getElementById('pauseButton').style.display = 'inline-block';
-  	song.src = songs[songIndex];
+    console.log(songIndex)
+    document.getElementById('playButton').style.display = 'none';
+    document.getElementById('pauseButton').style.display = 'inline-block';
+    song.src = songs[songIndex];
     song.play();
     songTitle.innerHTML = songTitles[songIndex];
     artist.innerHTML = 'Zara Larsson' 
     playing = false;
 
   } else {
-  	document.getElementById('pauseButton').style.display = 'none';
-  	document.getElementById('playButton').style.display = 'inline-block';
-  	song.pause();
-  	songTitle.innerHTML = songTitles[songIndex];
+    document.getElementById('pauseButton').style.display = 'none';
+    document.getElementById('playButton').style.display = 'inline-block';
+    song.pause();
+    songTitle.innerHTML = songTitles[songIndex];
     artist.innerHTML = 'Zara Larsson'
     playing = true;
     console.log(songIndex)
@@ -36,20 +36,20 @@ const playOrPause = () => {
 }
 
 song.addEventListener('ended', function(){
-	nextSong()
+  nextSong()
 })
 
 const nextSong = () => {
-	songIndex++;
-	if (songIndex > 2){
-		songIndex = 0;
-	}
+  songIndex++;
+  if (songIndex > 2){
+    songIndex = 0;
+  }
 
-	song.src = songs[songIndex];
-	songTitle.innerHTML = songTitles[songIndex];
+  song.src = songs[songIndex];
+  songTitle.innerHTML = songTitles[songIndex];
 
-	playing = true;
-	playOrPause(songIndex)
+  playing = true;
+  playOrPause(songIndex)
 }
 
 const previousSong = () => {
@@ -82,27 +82,27 @@ const changeProgressBar = () => {
 // add event listener to every button in top tracks section
 
 for (let i = 0; i < playTrack.length; i++){
-	playTrack[i].addEventListener('click', function(){
-		songIndex = i
-		console.log(songIndex)
-		playOrPause()
-	})
+  playTrack[i].addEventListener('click', function(){
+    songIndex = i
+    console.log(songIndex)
+    playOrPause()
+  })
 }
 
 // show volume bar when hovering over volume button and the bar itself */
 document.getElementById('volumeButton').addEventListener('mouseover', function(){
-	document.querySelector('.volume-bar-div').style.display = 'inline-block'
+  document.querySelector('.volume-bar-div').style.display = 'inline-block'
 })
 
 document.getElementById('volumeBar').addEventListener('mouseover', function(){
-	document.querySelector('.volume-bar-div').style.display = 'inline-block'
+  document.querySelector('.volume-bar-div').style.display = 'inline-block'
 })
 
 document.getElementById('volumeBar').addEventListener('mouseout', function(){
-	document.querySelector('.volume-bar-div').style.display = 'none'
+  document.querySelector('.volume-bar-div').style.display = 'none'
 })
 
 // upon moving the volume slider increase or decrease volume
 volumeSlider.addEventListener('mousemove', function(){
-	song.volume = volumeSlider.value/100
+  song.volume = volumeSlider.value/100
 })
