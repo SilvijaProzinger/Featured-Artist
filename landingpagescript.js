3312f14df7d170a5c2fae6cd95e462d4f9b0c56f
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2022-11-21 16:52:52 +0100

    misc fixes

--- landingpagescript.js
+++ landingpagescript.js
@@ -1,13 +1,27 @@
 const landingPageAnimation = function(){
     const landingButton = document.getElementById('explore')
 
+    const preloadImage = (source, destElem) => {
+        let image = new Image()  
+        image.src = source  
+        image.onload = function () {
+            let cssBackground = 'url(' + image.src + ')' 
+            destElem.style.backgroundImage = cssBackground
+        }
+    }
+
     //change background image to gif upon hovering over buttton
     landingButton.addEventListener('mouseover', function(){
-     document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/bg.gif')"
-        
+        document.getElementsByTagName("BODY")[0].classList.add('hovered')      
     })
     
     landingButton.addEventListener('mouseout', function(){
-     document.getElementsByTagName("BODY")[0].style.backgroundImage = "url('images/hanny-naibaho-aWXVxy8BSzc-unsplash.jpg')"
+        document.getElementsByTagName("BODY")[0].classList.remove('hovered')      
     }) 
+
+    return {
+        preloadImage: preloadImage
+    }
 }()
+
+landingPageAnimation.preloadImage('images/bg.gif', 'body');
