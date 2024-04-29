95bc2a4cecb9d7ffbd9b8fac87b36085f42141cd
SilvijaProzinger <silvija.prozinger@gmail.com>
2024-04-29 20:25:00 +0200

    format and edit main container width

--- landingpagescript.js
+++ landingpagescript.js
@@ -1,27 +1,27 @@
-const landingPageAnimation = function(){
-    const landingButton = document.getElementById('explore')
+const landingPageAnimation = (function () {
+  const landingButton = document.getElementById("explore");
 
-    const preloadImage = (source, destElem) => {
-        let image = new Image()  
-        image.src = source  
-        image.onload = function () {
-            let cssBackground = 'url(' + image.src + ')' 
-            destElem.style.backgroundImage = cssBackground
-        }
-    }
+  const preloadImage = (source, destElem) => {
+    let image = new Image();
+    image.src = source;
+    image.onload = function () {
+      let cssBackground = "url(" + image.src + ")";
+      destElem.style.backgroundImage = cssBackground;
+    };
+  };
 
-    //change background image to gif upon hovering over buttton
-    landingButton.addEventListener('mouseover', function(){
-        document.getElementsByTagName("BODY")[0].classList.add('hovered')      
-    })
-    
-    landingButton.addEventListener('mouseout', function(){
-        document.getElementsByTagName("BODY")[0].classList.remove('hovered')      
-    }) 
+  //change background image to gif upon hovering over buttton
+  landingButton.addEventListener("mouseover", function () {
+    document.getElementsByTagName("BODY")[0].classList.add("hovered");
+  });
 
-    return {
-        preloadImage: preloadImage
-    }
-}()
+  landingButton.addEventListener("mouseout", function () {
+    document.getElementsByTagName("BODY")[0].classList.remove("hovered");
+  });
 
-landingPageAnimation.preloadImage('images/bg.gif', 'body');
+  return {
+    preloadImage: preloadImage,
+  };
+})();
+
+landingPageAnimation.preloadImage("images/bg.gif", "body");
--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-const musicPlayer = function(){
+const musicPlayer = (function () {
   let playing = true;
-      song = document.getElementById('audio');
-      songTitle = document.getElementById('songTitle');
-      artist = document.getElementById('artistName');
-      progressBar = document.getElementById('progressBar');
+  song = document.getElementById("audio");
+  songTitle = document.getElementById("songTitle");
+  artist = document.getElementById("artistName");
+  progressBar = document.getElementById("progressBar");
 
-      songIndex = 0;
-      songs = ['music/bensound-energy.mp3', 'music/bensound-hey.mp3', 'music/bensound-perception.mp3']
-      songTitles = ["Lush Life", "Ruin My Life", "All the Time"];
+  songIndex = 0;
+  songs = [
+    "music/bensound-energy.mp3",
+    "music/bensound-hey.mp3",
+    "music/bensound-perception.mp3",
+  ];
+  songTitles = ["Lush Life", "Ruin My Life", "All the Time"];
 
-      playTrack = document.getElementsByClassName('play-track')
-      volumeSlider = document.getElementById('volumeSlider')
+  playTrack = document.getElementsByClassName("play-track");
+  volumeSlider = document.getElementById("volumeSlider");
 
   const playOrPause = () => {
     if (playing) {
-      document.getElementById('playButton').style.display = 'none';
-      document.getElementById('pauseButton').style.display = 'inline-block';
+      document.getElementById("playButton").style.display = "none";
+      document.getElementById("pauseButton").style.display = "inline-block";
       song.src = songs[songIndex];
       song.play();
       songTitle.innerHTML = songTitles[songIndex];
-      artist.innerHTML = 'Zara Larsson' 
+      artist.innerHTML = "Zara Larsson";
       playing = false;
-
     } else {
-      document.getElementById('pauseButton').style.display = 'none';
-      document.getElementById('playButton').style.display = 'inline-block';
+      document.getElementById("pauseButton").style.display = "none";
+      document.getElementById("playButton").style.display = "inline-block";
       song.pause();
       songTitle.innerHTML = songTitles[songIndex];
-      artist.innerHTML = 'Zara Larsson'
+      artist.innerHTML = "Zara Larsson";
       playing = true;
     }
-  }
+  };
 
   const nextSong = () => {
     songIndex++;
-    if (songIndex > 2){
+    if (songIndex > 2) {
       songIndex = 0;
     }
 
@@ -42,30 +45,30 @@ const musicPlayer = function(){
     songTitle.innerHTML = songTitles[songIndex];
 
     playing = true;
-    playOrPause(songIndex)
-  }
+    playOrPause(songIndex);
+  };
 
   const previousSong = () => {
-      songIndex--;
-      if (songIndex < 0) {
-          songIndex = 2;
-      };
+    songIndex--;
+    if (songIndex < 0) {
+      songIndex = 2;
+    }
 
-      song.src = songs[songIndex];
-      songTitle.innerHTML = songTitles[songIndex];
+    song.src = songs[songIndex];
+    songTitle.innerHTML = songTitles[songIndex];
 
-      playing = true;
-      playOrPause(songIndex);
-  }
+    playing = true;
+    playOrPause(songIndex);
+  };
 
-  song.addEventListener('ended', function(){
-    nextSong()
-  })
+  song.addEventListener("ended", function () {
+    nextSong();
+  });
 
   // update progressBar.max to display song duration
   const updateProgressValue = () => {
-      progressBar.max = song.duration;
-      progressBar.value = song.currentTime;
+    progressBar.max = song.duration;
+    progressBar.value = song.currentTime;
   };
 
   // update the progress bar as song goes on
@@ -73,40 +76,46 @@ const musicPlayer = function(){
 
   // change progressBar.value when dragging the progress bar slider
   const changeProgressBar = () => {
-      song.currentTime = progressBar.value;
-  }
+    song.currentTime = progressBar.value;
+  };
 
   // add event listener to every button in top tracks section
 
-  for (let i = 0; i < playTrack.length; i++){
-    playTrack[i].addEventListener('click', function(){
-      songIndex = i
-      console.log(songIndex)
-      playOrPause()
-    })
+  for (let i = 0; i < playTrack.length; i++) {
+    playTrack[i].addEventListener("click", function () {
+      songIndex = i;
+      console.log(songIndex);
+      playOrPause();
+    });
   }
 
   // show volume bar when hovering over volume button and the bar itself */
-  document.getElementById('volumeButton').addEventListener('mouseover', function(){
-    document.querySelector('.volume-bar-div').style.display = 'inline-block'
-  })
+  document
+    .getElementById("volumeButton")
+    .addEventListener("mouseover", function () {
+      document.querySelector(".volume-bar-div").style.display = "inline-block";
+    });
 
-  document.getElementById('volumeBar').addEventListener('mouseover', function(){
-    document.querySelector('.volume-bar-div').style.display = 'inline-block'
-  })
+  document
+    .getElementById("volumeBar")
+    .addEventListener("mouseover", function () {
+      document.querySelector(".volume-bar-div").style.display = "inline-block";
+    });
 
-  document.getElementById('volumeBar').addEventListener('mouseout', function(){
-    document.querySelector('.volume-bar-div').style.display = 'none'
-  })
+  document
+    .getElementById("volumeBar")
+    .addEventListener("mouseout", function () {
+      document.querySelector(".volume-bar-div").style.display = "none";
+    });
 
   // upon moving the volume slider increase or decrease volume
-  volumeSlider.addEventListener('mousemove', function(){
-    song.volume = volumeSlider.value/100
-  })
+  volumeSlider.addEventListener("mousemove", function () {
+    song.volume = volumeSlider.value / 100;
+  });
 
   return {
     playOrPause: playOrPause,
     nextSong: nextSong,
-    previousSong: previousSong
-  }
-}()
+    previousSong: previousSong,
+  };
+})();
